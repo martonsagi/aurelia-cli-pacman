2a46a3e7663e0fc23f41093f19e1266ea612361e
Márton Sági <hello@martonsagi.me>
2016-10-26 14:31:05 +0200

    Fix prepare-materialize task

--- tasks/prepare-materialize.js
+++ tasks/prepare-materialize.js
@@ -1,16 +1,22 @@
 import gulp from 'gulp';
-import merge from 'merge-stream';
-import * as project from '../aurelia.json';
-import path from 'path';
 
-export default function prepareMaterialize() {
-  let source = 'node_modules/materialize-css';
+let source = './node_modules/materialize-css';
 
-  let taskCss = gulp.src(path.join(source, 'dist/css/materialize.min.css'))
-    .pipe(gulp.dest(path.join(project.platform.output, '../', 'styles')));
+let taskCss = () => {
+    return gulp
+        .src([`${source}/dist/css/*.min.css`])
+        .pipe(gulp
+            .dest('styles')
+        );
+};
 
-  let taskFonts = gulp.src(path.join(source, 'dist/fonts/roboto/*'))
-    .pipe(gulp.dest(path.join(project.platform.output, '../', 'fonts/roboto')));
+let taskFonts = () => {
+    return gulp
+        .src([`${source}/dist/fonts/roboto/*`])
+        .pipe(gulp
+            .dest('fonts/roboto')
+        );
+};
 
-  return merge(taskCss, taskFonts);
-}
+export default gulp.series(gulp.parallel.apply(gulp, [taskFonts, taskCss]));
+
